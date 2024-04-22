289a9ed686b5d6553a54518b08911dae61826761
Maksim Bolotin <vk07122012@gmail.com>
2024-04-22 22:09:41 +0300

    Сделана настройка URL-файлов для нового приложения.  Подготовлены два шаблона для домашней страницы и страницы с контактной информацией.

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_page(request):
+    """Контроллер для отображения домашней страницы."""
+    # products = Product.objects.all()
+    return render(request, 'home.html')
+
+
+def contact_page(request):
+    """Контроллер для отображения страницы с контактной информацией."""
+    return render(request, 'contact.html')
