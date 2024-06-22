fadc3e65a8f41d75b9e47d951589c0507ec7b242
Maksim Bolotin <vk07122012@gmail.com>
2024-06-22 15:14:39 +0300

    Исправлены урлы, добавлен контролер для отображения продуктов по категориям, обновлены шаблоны.

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalog.urls')),
+    path('', include('catalog.urls', namespace="products")),
     path('blogpost/', include('blogpost.urls', namespace='blogpost')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
